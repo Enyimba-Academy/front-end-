39a60579ef11a539a0485454c6fbdee3dbd5be0e
alidauda <alidauda14@gmail.com>
2025-05-07 18:21:06 +0100

    Update API base URL from HTTP to HTTPS for improved security.

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import useAuthStore from "@/store/authStore";
 
 const api = axios.create({
-  baseURL: "http://24.199.124.67:4000/api",
+  baseURL: "https://24.199.124.67:4000/api",
 });
 
 api.interceptors.request.use(
